929ef0991b588cca073a104238905f372ef62bd8
ywcYY <115331253+ywcYY@users.noreply.github.com>
2023-05-22 18:43:19 +0800

    Gin  test  个人博客

--- Gin_First/gin.go
+++ Gin_First/gin.go
@@ -1,14 +1,28 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	Username string `uri:"username"`
+	Password string `uri:"password"`
+}
+
+func TestGetBind(c *gin.Context) {
+	var user User
+	err := c.ShouldBindUri(&user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c.String(200, "%v", user)
+}
 
 func main() {
-
-	engine := gin.Default()
-	engine.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	engine.Run()
+	e := gin.Default()
+	// http://localhost:8080/testGetBind/ywc/123
+	e.GET("/testGetBind/:username/:password", TestGetBind)
+	e.Run()
 }
